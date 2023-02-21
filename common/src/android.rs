ab5738881f3a125df8415d3e6934d15e756166f2
Michael Pöhn <michael@poehn.at>
2023-02-21 02:48:12 +0100

    📤 upgrade and migrate to jni-rs 0.21

--- common/src/android.rs
+++ common/src/android.rs
@@ -13,21 +13,28 @@ use jni::JNIEnv;
 /// Create a static method myMethod on class net.example.MyClass
 #[no_mangle]
 #[allow(non_snake_case)]
-pub extern "system" fn Java_info_guardianproject_arti_ArtiJNI_startArtiProxyJNI(
-    env: JNIEnv,
-    _class: JClass,
-    cacheDir: JString,
-    stateDir: JString,
+pub extern "system" fn Java_info_guardianproject_arti_ArtiJNI_startArtiProxyJNI<'local>(
+    mut env: JNIEnv<'local>,
+    _class: JClass<'local>,
+    cacheDir: JString<'local>,
+    stateDir: JString<'local>,
     socks_port: jint,
     dns_port: jint,
 ) -> jstring {
+    let cacheDir: String = env
+        .get_string(&cacheDir)
+        .expect("cache_dir is invalid")
+        .to_string_lossy()
+        .into_owned();
+    let stateDir: String = env
+        .get_string(&stateDir)
+        .expect("state_dir is invalid")
+        .to_string_lossy()
+        .into_owned();
+
     let result = match start_arti_proxy(
-        &env.get_string(cacheDir)
-            .expect("cache_dir is invalid")
-            .to_string_lossy(),
-        &env.get_string(stateDir)
-            .expect("state_dir is invalid")
-            .to_string_lossy(),
+        &cacheDir,
+        &stateDir,
         socks_port as u16,
         dns_port as u16,
         move |_buf: &[u8]| {},
@@ -44,9 +51,9 @@ pub extern "system" fn Java_info_guardianproject_arti_ArtiJNI_startArtiProxyJNI(
 // this is supposed to forward arti's built-in logging to logcat
 // https://gitlab.torproject.org/tpo/core/arti/-/blob/main/doc/Android.md#debugging-and-stability
 #[no_mangle]
-pub extern "system" fn Java_info_guardianproject_arti_ArtiJNI_initLogging(
-    _env: JNIEnv,
-    _class: JClass,
+pub extern "system" fn Java_info_guardianproject_arti_ArtiJNI_initLogging<'local>(
+    mut _env: JNIEnv<'local>,
+    _class: JClass<'local>,
 ) {
     let layer = tracing_android::layer("rust.arti").expect("couldn't create tracing layer");
     Subscriber::new().with(layer).init(); // this must be called only once, otherwise your app will probably crash
