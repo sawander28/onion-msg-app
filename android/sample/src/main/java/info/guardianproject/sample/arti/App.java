905a106c624bcad915bd67d34495b11cc233457a
Michael Pöhn <michael@poehn.at>
2024-05-06 18:38:39 +0200

    🪩🪩 include IPtProxy dependency

--- android/sample/src/main/java/info/guardianproject/sample/arti/App.java
+++ android/sample/src/main/java/info/guardianproject/sample/arti/App.java
@@ -4,8 +4,13 @@
 package info.guardianproject.sample.arti;
 
 import android.app.Application;
+import android.util.Log;
+
+import java.io.File;
+import IPtProxy.IPtProxy;
 
 import info.guardianproject.arti.Arti;
+import info.guardianproject.arti.ArtiProxy;
 
 public class App extends Application {
     @Override
@@ -18,8 +23,20 @@ public class App extends Application {
          * Arti.wrapWebView();
          */
 
-        //this does the three steps above
-        Arti.init(this);
+        File ptDir = new File(getCacheDir(), "pt_state");
+        IPtProxy.setStateLocation(ptDir.getAbsolutePath());
+        Log.d("###", String.format("obfs2 port: %d", IPtProxy.obfs2Port()));
+        Log.d("###", String.format("obfs3 port: %d", IPtProxy.obfs3Port()));
+        Log.d("###", String.format("obfs4 port: %d", IPtProxy.obfs4Port()));
+        Log.d("###", String.format("obfs4 version: %s", IPtProxy.obfs4ProxyVersion()));
 
+        Log.d("###", String.format("snowflake port: %d", IPtProxy.snowflakePort()));
+        Log.d("###", String.format("snowflake version: %s", IPtProxy.snowflakeVersion()));
+
+        //this does the three steps above
+        // Arti.init(this);
+
+        ArtiProxy artiProxy = ArtiProxy.Builder(this).build();
+        artiProxy.start();
     }
 }
