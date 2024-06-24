b0198add53dbd6e92558eaa0b7524c05dfde71d1
Vasila Mirshamsova <vasila.mirshamsova@gmail.com>
2024-06-24 18:40:43 +0500

    switching works, tor direct

--- android/sample/src/main/java/info/guardianproject/sample/arti/App.java
+++ android/sample/src/main/java/info/guardianproject/sample/arti/App.java
@@ -5,6 +5,7 @@ package info.guardianproject.sample.arti;
 
 import android.app.Application;
 import android.util.Log;
+import android.view.View;
 
 import java.io.File;
 import java.util.Arrays;
@@ -19,16 +20,16 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
 
-        Log.d("###", String.format("obfs2 port: %d", IPtProxy.obfs2Port()));
-        Log.d("###", String.format("obfs3 port: %d", IPtProxy.obfs3Port()));
-        Log.d("###", String.format("obfs4 port: %d", IPtProxy.obfs4Port()));
-        Log.d("###", String.format("lyrebird version: %s", IPtProxy.lyrebirdVersion()));
-        Log.d("###", String.format("snowflake port: %d", IPtProxy.snowflakePort()));
-        Log.d("###", String.format("snowflake version: %s", IPtProxy.snowflakeVersion()));
+//        Log.d("###", String.format("obfs2 port: %d", IPtProxy.obfs2Port()));
+//        Log.d("###", String.format("obfs3 port: %d", IPtProxy.obfs3Port()));
+//        Log.d("###", String.format("obfs4 port: %d", IPtProxy.obfs4Port()));
+//        Log.d("###", String.format("lyrebird version: %s", IPtProxy.lyrebirdVersion()));
+//        Log.d("###", String.format("snowflake port: %d", IPtProxy.snowflakePort()));
+//        Log.d("###", String.format("snowflake version: %s", IPtProxy.snowflakeVersion()));
 
         IPtProxy.setStateLocation(new File(getCacheDir(), "pt_state").getAbsolutePath());
         // run obfs4/lyrebird client
-        IPtProxy.startLyrebird("DEBUG", false, false, null);
+//        IPtProxy.startLyrebird("DEBUG", false, false, null);
 
         // run snowflake client (values copied from )
         // TODO: fix this, once we've updated to the latest iptproxy version
@@ -51,18 +52,43 @@ public class App extends Application {
                 1 // long maxPeers
         );
 
+//        List<String> bridgeLines = Arrays.asList(
+//                // NOTICE: you'll need to provide bridge lines to make this work!
+//                "snowflake 192.0.2.3:80 2B280B23E1107BB62ABFC40DDCC8824814F80A72 fingerprint=2B280B23E1107BB62ABFC40DDCC8824814F80A72 url=https://snowflake-broker.torproject.net.global.prod.fastly.net/ front=github.githubassets.com ice=stun:stun.l.google.com:19302,stun:stun.antisip.com:3478,stun:stun.bluesip.net:3478,stun:stun.dus.net:3478,stun:stun.epygi.com:3478,stun:stun.sonetel.com:3478,stun:stun.uls.co.za:3478,stun:stun.voipgate.com:3478,stun:stun.voys.nl:3478 utls-imitate=hellorandomizedalpn",
+//                "snowflake 192.0.2.4:80 8838024498816A039FCBBAB14E6F40A0843051FA fingerprint=8838024498816A039FCBBAB14E6F40A0843051FA url=https://snowflake-broker.torproject.net.global.prod.fastly.net/ front=github.githubassets.com ice=stun:stun.l.google.com:19302,stun:stun.antisip.com:3478,stun:stun.bluesip.net:3478,stun:stun.dus.net:3478,stun:stun.epygi.com:3478,stun:stun.sonetel.net:3478,stun:stun.uls.co.za:3478,stun:stun.voipgate.com:3478,stun:stun.voys.nl:3478 utls-imitate=hellorandomizedalpn"
+//        );
+
         // initialize and start ArtiProxy
-        List<String> bridgeLines = Arrays.asList(
-            // NOTICE: you'll need to provide bridge lines to make this work!
-            "snowflake 192.0.2.3:80 2B280B23E1107BB62ABFC40DDCC8824814F80A72 fingerprint=2B280B23E1107BB62ABFC40DDCC8824814F80A72 url=https://snowflake-broker.torproject.net.global.prod.fastly.net/ front=github.githubassets.com ice=stun:stun.l.google.com:19302,stun:stun.antisip.com:3478,stun:stun.bluesip.net:3478,stun:stun.dus.net:3478,stun:stun.epygi.com:3478,stun:stun.sonetel.com:3478,stun:stun.uls.co.za:3478,stun:stun.voipgate.com:3478,stun:stun.voys.nl:3478 utls-imitate=hellorandomizedalpn",
-            "snowflake 192.0.2.4:80 8838024498816A039FCBBAB14E6F40A0843051FA fingerprint=8838024498816A039FCBBAB14E6F40A0843051FA url=https://snowflake-broker.torproject.net.global.prod.fastly.net/ front=github.githubassets.com ice=stun:stun.l.google.com:19302,stun:stun.antisip.com:3478,stun:stun.bluesip.net:3478,stun:stun.dus.net:3478,stun:stun.epygi.com:3478,stun:stun.sonetel.net:3478,stun:stun.uls.co.za:3478,stun:stun.voipgate.com:3478,stun:stun.voys.nl:3478 utls-imitate=hellorandomizedalpn"
-        );
+
+//        ArtiProxy artiProxy = ArtiProxy.Builder(this)
+//                // .setUnmanagedSnowflakeClientPort((int) IPtProxy.snowflakePort())
+//                .setBridgeLines(bridgeLines)
+//                .setSnowflakePort((int) IPtProxy.snowflakePort())
+//                .setLogListener((log) -> {Log.e("artilog", log);})
+//                .build();
+//        artiProxy.start();
+    }
+
+    public void connectTorDirect() {
         ArtiProxy artiProxy = ArtiProxy.Builder(this)
                 // .setUnmanagedSnowflakeClientPort((int) IPtProxy.snowflakePort())
-                .setBridgeLines(bridgeLines)
+//                .setSnowflakePort((int) IPtProxy.snowflakePort())
+                .setLogListener((log) -> {Log.e("artilog", log);})
+                .build();
+        artiProxy.start();
+    }
+
+    public void connectWithLyrebird(int port, List<String> bridgeLines) {
+        IPtProxy.startLyrebird("DEBUG", false, false, null);
+
+        ArtiProxy artiProxy = ArtiProxy.Builder(this)
+                // .setUnmanagedSnowflakeClientPort((int) IPtProxy.snowflakePort())
+                //.setObfs4Port()
+                //.setBridgeLines()
                 .setSnowflakePort((int) IPtProxy.snowflakePort())
                 .setLogListener((log) -> {Log.e("artilog", log);})
                 .build();
         artiProxy.start();
     }
+
 }
--- android/sample/src/main/java/info/guardianproject/sample/arti/MainActivity.java
+++ android/sample/src/main/java/info/guardianproject/sample/arti/MainActivity.java
@@ -7,44 +7,100 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
 import android.view.ViewPropertyAnimator;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.io.File;
 import java.lang.ref.WeakReference;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
-import info.guardianproject.sample.arti.R;
+import IPtProxy.IPtProxy;
+import info.guardianproject.arti.ArtiProxy;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
     private FloatingActionButton fab;
     private ViewPropertyAnimator fabSpin;
-    private Spinner spinner;
+    private EditText bridgeLineInput;
+    private EditText obfs4Port;
+    private EditText stunServerInput;
+    private EditText targetInput;
+    private EditText frontInput;
+    private TextView noSelection;
+    private TextView connectionStatus;
+    private CountDownLatch latch;
+    private Handler mainHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        mainHandler = new Handler(Looper.getMainLooper());
+
         textView = findViewById(R.id.text);
         textView.setText(getString(R.string.intro_text));
 
         fab = findViewById(R.id.floatingActionButton);
-        fab.setOnClickListener(view -> checkConnection());
+        fab.setOnClickListener(view -> {
+//            if (latch != null) {
+//                latch.countDown();
+//            }
+            checkConnection();
+        });
         fabSpin = fab.animate();
 
-        spinner = findViewById(R.id.spinner);
-    }
+        Spinner spinner = findViewById(R.id.spinner);
+        bridgeLineInput = findViewById(R.id.bridgeLineInput);
+        stunServerInput = findViewById(R.id.stunServerInput);
+        targetInput = findViewById(R.id.targetInput);
+        frontInput = findViewById(R.id.frontInput);
+        noSelection = findViewById(R.id.no_option_selected);
+        connectionStatus = findViewById(R.id.status);
+        obfs4Port = findViewById(R.id.obfs4Port);
 
+        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this, R.array.connection_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                new Thread(new Runnable() {
+//                    @Override
+//                    public void run() {
+                onSelectionChanged(position); // get options
+//                    }
+//                }).start();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing
+            }
+        });
+    }
     private void checkConnection() {
         fab.setEnabled(false);
         fabSpin.setDuration(1000*60).rotationBy((float) (1000 * 60) /4).setInterpolator(new LinearInterpolator()).start();
-        textView.setText(R.string.performing_request);
+        connectionStatus.setVisibility(View.VISIBLE);
+        connectionStatus.setText(R.string.performing_request);
 
         new CheckTorConnectionTask(this).execute();
     }
@@ -80,15 +136,81 @@ public class MainActivity extends AppCompatActivity {
         fab.setEnabled(true);
         switch (s) {
             case DIRECT:
-                textView.setText(R.string.tor_no_arti);
+                connectionStatus.setText(R.string.tor_no_arti);
                 break;
             case TOR:
-                textView.setText(R.string.tor_with_arti);
+                connectionStatus.setText(R.string.tor_with_arti);
                 break;
             case ERROR:
-                textView.setText(R.string.connection_failed);
+                connectionStatus.setText(R.string.connection_failed);
                 break;
         }
         fab.setActivated(true);
     }
+
+    private void onSelectionChanged(int selection) {
+        // Update the UI or perform some action based on the selection
+        switch (selection) {
+            case 0:
+                stunServerInput.setVisibility(View.GONE);
+                targetInput.setVisibility(View.GONE);
+                frontInput.setVisibility(View.GONE);
+                bridgeLineInput.setVisibility(View.GONE);
+                obfs4Port.setVisibility(View.GONE);
+                noSelection.setVisibility(View.VISIBLE);
+                break;
+            case 1:
+                // do nothing
+                stunServerInput.setVisibility(View.GONE);
+                obfs4Port.setVisibility(View.GONE);
+                targetInput.setVisibility(View.GONE);
+                frontInput.setVisibility(View.GONE);
+                bridgeLineInput.setVisibility(View.GONE);
+                noSelection.setVisibility(View.GONE);
+                ((App)getApplication()).connectTorDirect();
+                break;
+            case 2:
+                bridgeLineInput.setVisibility(View.VISIBLE);
+                obfs4Port.setVisibility(View.VISIBLE);
+                stunServerInput.setVisibility(View.GONE);
+                targetInput.setVisibility(View.GONE);
+                frontInput.setVisibility(View.GONE);
+                noSelection.setVisibility(View.GONE);
+//                waitForButtonClick();
+//                Log.d("DEBUG", "onSelectionChanged: got in case 2");
+//                mainHandler.post(new Runnable() {
+//                    @Override
+//                    public void run() {
+                        List<String> bridgeLines = Arrays.asList(
+                                // NOTICE: you'll need to provide bridge lines to make this work!
+                                //obfs4 69.235.46.22:30913 F79914011EB368C94E58F6CCF8A55A92EFD5F496 cert=ZKLm+4biqgPIf/g1s3slv8jLSzIzLSXAHFOfBLqtrNvnTM6LVbxe/K8e8jJKiXwOpvkoDw iat-mode=0
+                                //obfs4 82.74.251.112:9449 628B95EEAE48758CBAA2812AE99E1AB5B3BE44D4 cert=i7tmgWvq4X2rncJz4FQsQWwkXiEWVE7Nvm1gffYn5ZlVsA0kBF6c/8041dTB4mi0TSShWA iat-mode=0
+                                bridgeLineInput.getText().toString()
+                        );
+
+//                        ((App) getApplication()).connectWithLyrebird(Integer.parseInt(obfs4Port.getText().toString()),
+//                                              bridgeLines);
+//                    }
+//                });
+                break;
+            case 3:
+                bridgeLineInput.setVisibility(View.VISIBLE);
+                stunServerInput.setVisibility(View.VISIBLE);
+                targetInput.setVisibility(View.VISIBLE);
+                frontInput.setVisibility(View.VISIBLE);
+                noSelection.setVisibility(View.GONE);
+                obfs4Port.setVisibility(View.GONE);
+                break;
+        }
+    }
+
+//    private void waitForButtonClick() {
+//        latch = new CountDownLatch(1);
+//        try {
+//            latch.await(); // Wait until the button is clicked
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
 }
